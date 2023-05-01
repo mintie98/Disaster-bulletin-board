41c79a09e9b04f8c2497dbb4298ea867d25481a7
清水 <noon3m.op.sh@gmail.com>
2023-05-01 09:57:12 +0900

    homeのuser側の修正

--- board_home.php
+++ board_home.php
@@ -1,67 +1,99 @@
 <?php
-require_once 'function.php';
+// 情報をデータベースに入れる
+// setcookie('login_conf', date('Y-m-d H:i:s'));
+
+require_once dirname(__FILE__) . '/function/function.php';
 $a = true;
+$admin = true;
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
+<!-- 会員の方はこちら -->
 
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>災害用掲示板</title>
-  <!-- bootstrap -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+  <title>掲示板</title>
+  <!-- tailwind css -->
+  <link href="./css/output.css" rel="stylesheet">
   <!-- icon -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <style>
+    body {
+      /* background-color: #E9722E; */
+      background-color: #80BB6B;
+    }
+
+    /* .absolute {
+      color: #E9722E;
+    } */
+
+    .bg-btn {
+      /* background-color: #80BB6B; */
+      background-color: #FFFFFF;
+    }
+  </style>
 </head>
 
 <body>
   <header>
     <?php
-    if ($a) {
-      include 'navbar_admin.php';
-    } else {
-      include 'navbar_user.php';
-    }
+    // if ($a) {
+    include 'navbar_admin.php';
+    // } else {
+    //   include 'navbar_user.php';
+    // }
+    // 
     ?>
   </header>
-  //TODO ドロップダウン その他
-  <main class="container">
-    <div class="row justify-content-center">
-      <h2 class="text-center mt-3 mb-5">災害情報</h2>
-      <!-- 画面遷移 -->
-      <div style="width:75%; height:20vh" class="row p-0 m-0">
-        <div class="col btn btn-primary text-white d-flex align-items-center justify-content-center" onclick="location.href='./post.html'">
-          <div class="h1 m-0">安否確認</div>
-        </div>
-        <div class="col btn btn-primary text-white ms-3 d-flex align-items-center justify-content-center" onclick="location.href='./post.html'">
-          <div class="h1 m-0">業務連絡</div>
-        </div>
-      </div>
+
+  <main class="container mx-auto">
+    <div class="justify-center items-">
+      <!-- user nemu -->
       <?php
       if ($a) {
       ?>
-        <div style="width:75%; height:20vh" class="row p-0 m-0 mt-3">
-          <div class="col btn btn-danger text-white d-flex align-items-center justify-content-center" onclick="location.href='./arrival_admin.php'">
-            <div class="h1 m-0">出社状況</div>
-          </div>
-          <div class="col btn btn-danger text-white ms-3 d-flex align-items-center justify-content-center" onclick="location.href='./safety_admin.php'">
-            <div class="h1 m-0">安否状況</div>
-          </div>
+        <div class="relative flex justify-center items-center my-16 mx-auto text-black">
+          <i class="absolute -top-10 left-2 text-7xl fa fa-file-pen"></i>
+          <button class="bg-btn w-10/12 h-52 rounded-lg flex items-center justify-center" onclick="location.href='./disaster.php'">
+            <ruby>
+              <span class="m-0 text-9xl font-body font-bold tracking-widest">報告</span>
+              <rt class="m-0 text-3xl font-body font-bold">ほうこく</rt>
+            </ruby>
+          </button>
         </div>
+        <div class="relative flex justify-center items-center mb-3 text-black">
+          <i class="absolute -top-10 left-2 text-7xl fa-solid fa-list"></i>
+          <button class="bg-btn w-10/12 h-52 rounded-lg flex items-center justify-center" onclick="location.href='./all_info.php'">
+            <ruby>
+              <span class="m-0 text-9xl font-body font-bold tracking-widest">一覧</span>
+              <rt class="m-0 text-3xl font-body font-bold">いちらん</rt>
+            </ruby>
+          </button>
+        </div>
+      <?php
+      }
+      ?>
+      <!-- admin nemu -->
+      <?php
+      if (!$a) {
+      ?>
+        <div class="flex flex-wrap justify-center items-center mb-3 mx-auto text-black">
+          <button class="bg-btn w-40 h-40 p-5 rounded-lg flex items-center justify-center" <?= $admin ? '' : 'disabled' ?> onclick="location.href='./disaster.php'">
+            <div class="m-0 text-3xl">災害時<br> 安否確認</div>
+          </button>
+          <button class="bg-btn w-40 h-40 p-5 rounded-lg  ms-3 flex items-center justify-center" <?= $admin ? '' : 'disabled' ?> onclick="location.href='./all_info.php'">
+            <div class="m-0 text-3xl">一覧</div>
+          </button>
+        </div>
+
       <?php
       }
       ?>
     </div>
   </main>
-  <footer style="height: 5vh;" class="fixed-bottom bg-secondary">
-    <div class="d-flex justify-content-around align-items-center h-100">
-      <a href="" class="text-white"><i class="fas fa-home"></i></a>
-      <a href="" class="text-white"><i class="fas fa-comment"></i></a>
-    </div>
-  </footer>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  <script src="./js/navbar.js"></script>
 </body>
 
 </html>
